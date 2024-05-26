6745869628cfcc1e2f7d74d2cac424962a48c53f
H0llyW00dzZ <priv8@btz.pm>
2024-05-26 18:11:00 +0700

    Implement [Crypto] [Hash Function] BLAKE2b
    - [+] feat(go.mod): add golang.org/x/crypto dependency
    - [+] feat(internal/crypto/hash): implement Blake2b-based hash for OTP
    - [+] Add Blake2botp struct that wraps BLAKE2b hash function
    - [+] Implement hash.Hash interface for Blake2botp
    - [+] Provide New512 function to create a new instance of Blake2botp with 512-bit output size
    - [+] Add tests for TOTP and HOTP using Blake2botp hash
    - [+] Include package documentation for blake2botp package

---
+++ internal/crypto/hash/blake2b.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package blake2botp
+
+import (
+	"hash"
+
+	"golang.org/x/crypto/blake2b"
+)
+
+// New512 returns a new instance of the Blake2botp hash with a 512-bit output size.
+// The returned hash implements the hash.Hash interface.
+func New512() hash.Hash {
+	h, _ := blake2b.New512(nil)
+	return &Blake2botp{
+		hash: h,
+	}
+}
+
+// Blake2botp is a struct that wraps the BLAKE2b hash function.
+// It implements the hash.Hash interface.
+type Blake2botp struct {
+	hash hash.Hash
+}
+
+// Write writes the given bytes to the hash.
+func (h *Blake2botp) Write(p []byte) (n int, err error) {
+	return h.hash.Write(p)
+}
+
+// Sum appends the current hash to b and returns the resulting slice.
+func (h *Blake2botp) Sum(b []byte) []byte {
+	return h.hash.Sum(b)
+}
+
+// Reset resets the hash to its initial state.
+func (h *Blake2botp) Reset() {
+	h.hash.Reset()
+}
+
+// Size returns the number of bytes Sum will return.
+func (h *Blake2botp) Size() int {
+	return h.hash.Size()
+}
+
+// BlockSize returns the hash's underlying block size.
+func (h *Blake2botp) BlockSize() int {
+	return h.hash.BlockSize()
+}
