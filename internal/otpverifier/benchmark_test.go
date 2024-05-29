2bcb26b5205ce489ca7440775ef638adc8195b11
H0llyW00dzZ <priv8@btz.pm>
2024-05-30 06:40:12 +0700

    Benchmark Hash Function
    - [+] feat(otpverifier): add benchmark tests for TOTP and HOTP verification

---
+++ internal/otpverifier/benchmark_test.go
@@ -0,0 +1,120 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package otpverifier_test
+
+import (
+	"testing"
+
+	"github.com/H0llyW00dzZ/fiber2fa/internal/otpverifier"
+	"github.com/xlzd/gotp"
+)
+
+func BenchmarkTOTPVerify(b *testing.B) {
+	secret := gotp.RandomSecret(16)
+
+	hashFunctions := []string{
+		otpverifier.SHA1,
+		otpverifier.SHA224,
+		otpverifier.SHA256,
+		otpverifier.SHA384,
+		otpverifier.SHA512,
+		otpverifier.SHA512S224,
+		otpverifier.SHA512S256,
+		otpverifier.BLAKE2b256,
+		otpverifier.BLAKE2b384,
+		otpverifier.BLAKE2b512,
+		otpverifier.BLAKE3256,
+		otpverifier.BLAKE3384,
+		otpverifier.BLAKE3512,
+	}
+
+	for _, hashFunc := range hashFunctions {
+		b.Run(hashFunc, func(b *testing.B) {
+			config := otpverifier.Config{
+				Secret: secret,
+				Hash:   hashFunc,
+			}
+			verifier := otpverifier.NewTOTPVerifier(config)
+
+			token, _ := verifier.GenerateToken()
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				verifier.Verify(token, "")
+			}
+		})
+
+		b.Run(hashFunc+"_WithSignature", func(b *testing.B) {
+			config := otpverifier.Config{
+				Secret:       secret,
+				SyncWindow:   1,
+				Hash:         hashFunc,
+				UseSignature: true,
+			}
+			verifier := otpverifier.NewTOTPVerifier(config)
+
+			token, signature := verifier.GenerateToken()
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				verifier.Verify(token, signature)
+			}
+		})
+	}
+}
+
+func BenchmarkHOTPVerify(b *testing.B) {
+	secret := gotp.RandomSecret(16)
+
+	hashFunctions := []string{
+		otpverifier.SHA1,
+		otpverifier.SHA224,
+		otpverifier.SHA256,
+		otpverifier.SHA384,
+		otpverifier.SHA512,
+		otpverifier.SHA512S224,
+		otpverifier.SHA512S256,
+		otpverifier.BLAKE2b256,
+		otpverifier.BLAKE2b384,
+		otpverifier.BLAKE2b512,
+		otpverifier.BLAKE3256,
+		otpverifier.BLAKE3384,
+		otpverifier.BLAKE3512,
+	}
+
+	for _, hashFunc := range hashFunctions {
+		b.Run(hashFunc, func(b *testing.B) {
+			config := otpverifier.Config{
+				Secret:     secret,
+				SyncWindow: 1,
+				Hash:       hashFunc,
+			}
+			verifier := otpverifier.NewHOTPVerifier(config)
+
+			token, _ := verifier.GenerateToken()
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				verifier.Verify(token, "")
+			}
+		})
+
+		b.Run(hashFunc+"_WithSignature", func(b *testing.B) {
+			config := otpverifier.Config{
+				Secret:       secret,
+				Hash:         hashFunc,
+				UseSignature: true,
+			}
+			verifier := otpverifier.NewHOTPVerifier(config)
+
+			token, signature := verifier.GenerateToken()
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				verifier.Verify(token, signature)
+			}
+		})
+	}
+}
