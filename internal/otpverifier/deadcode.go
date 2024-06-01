3c4540d83271c5d33c17d559b233037b42b0b958
H0llyW00dzZ <priv8@btz.pm>
2024-06-01 13:37:42 +0700

    Docs [pkg.go.dev] Update Documentation related to deadcode (#109)
    - [+] refactor(otpverifier): deprecate SetCounter and GetCounter methods for TOTPVerifier

--- internal/otpverifier/deadcode.go
+++ internal/otpverifier/deadcode.go
@@ -5,11 +5,15 @@
 package otpverifier
 
 // SetCounter is a no-op for TOTPVerifier since TOTP doesn't use a counter.
+//
+// Deprecated: No-op for TOTP (deadcode)
 func (v *TOTPVerifier) SetCounter(counter uint64) {
 	// No-op for TOTP
 }
 
 // GetCounter always returns 0 for TOTPVerifier since TOTP doesn't use a counter.
+//
+// Deprecated: No-op for TOTP (deadcode)
 func (v *TOTPVerifier) GetCounter() uint64 {
 	return 0
 }
