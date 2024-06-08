4f973d3610a658ecc8c0d8c88c627f9481d85625
H0llyW00dzZ <priv8@btz.pm>
2024-06-08 22:36:17 +0700

    Docs [HOTP] [Helper Function] Update Comment
    - [+] refactor(otpverifier): defer resynchronization of HOTP verifier after successful verification

--- internal/otpverifier/hotp.go
+++ internal/otpverifier/hotp.go
@@ -333,6 +333,10 @@ func (v *HOTPVerifier) deferResynchronization(matchedCounter uint64) {
 		v.SetCounter(matchedCounter + 1)
 
 		// Update the last resynchronization time
+		//
+		// TODO: Bound to the [TimeSource] field which can be customized (e.g., use Monotonic Clock or Wall Clock). However, it is not really needed right now
+		// because most use cases prefer TOTP since it is stable and cheaper compared to HOTP,
+		// which mostly involves expensive allocations.
 		v.lastResyncTime = time.Now()
 
 		// Adjust the sync window
